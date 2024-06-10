53ae8beb796e5f365bef28029f584b70a2e0ef1b
Noam Dahan <noam.dahan1@cs.huji.ac.il>
2024-06-10 12:09:49 +0300

    test

--- instructions_and_examples.py
+++ instructions_and_examples.py
@@ -19,6 +19,7 @@ def next_page():
 
 
 def instructions_page():
+    st.title('Machine-Translation Evaluation: Instructions')
     st.markdown(INSTRUCTIONS)
     #st.markdown(EXAMPLES)
     st.button('Start training', key='next_button2', on_click=next_page)
