c527859b61b8938c3ebd581260044f8788094105
Noam Dahan <noam.dahan1@cs.huji.ac.il>
2024-06-10 11:44:00 +0300

    include training

--- instructions_and_examples.py
+++ instructions_and_examples.py
@@ -1,7 +1,7 @@
 
 import streamlit as st
 
-INSTRUCTIONS = """For each sentence in English, you will be presented with a Hebrew translation that is the output of a state-of-the-art machine translation model.
+INSTRUCTIONS = """For each sentence in English, you will be presented with a Hebrew translation that is the output of a machine translation model.
 
 Unfortunately, some of the translations might include errors that need corrections.
 
@@ -14,13 +14,13 @@ EXAMPLES = """<Examples for the task>.\n"""
 
 
 def next_page():
-    #st.session_state.cur_page = 'training'
-    st.session_state.cur_page = 'experiment'
+    st.session_state.cur_page = 'training'
+    #st.session_state.cur_page = 'experiment'
 
 
 def instructions_page():
     st.markdown(INSTRUCTIONS)
     #st.markdown(EXAMPLES)
-    #st.button('Start training', key='next_button2', on_click=next_page)
-    st.button('Start', key='next_button2', on_click=next_page)
+    st.button('Start training', key='next_button2', on_click=next_page)
+    #st.button('Start', key='next_button2', on_click=next_page)
 
